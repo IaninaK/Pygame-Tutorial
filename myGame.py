d2fa6a18f56626add177b45541faae9ee2f2d9b0
Kot Jason <kotjasonyk@gmail.com>
2022-03-19 18:49:03 -0700

    Lesson 1

---
+++ myGame.py
@@ -0,0 +1,15 @@
+import pygame
+pygame.init()
+
+pygame.display.set_mode((600, 400), pygame.RESIZABLE)
+pygame.display.set_caption("Epic Window")
+clock = pygame.time.Clock()
+FPS = 60
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    clock.tick(60)
---
+++ template.py
@@ -0,0 +1,14 @@
+import pygame
+pygame.init()
+
+pygame.display.set_mode((600, 400))
+
+clock = pygame.time.Clock()
+FPS = 60
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    clock.tick(60)
